be797f49a526d8bd2670b3bd94d18fd9b1cd1149
Khalil-Balde <KHALIL12345385@gmail.com>
2019-01-21 03:13:50 -0500

    added sticky clearing by default

--- src/main/java/frc/robot/commands/defaultTechnicalCommand.java
+++ src/main/java/frc/robot/commands/defaultTechnicalCommand.java
@@ -1,8 +1,21 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
 public class defaultTechnicalCommand extends Command {
+    public defaultTechnicalCommand(){
+        requires(Robot.getTechnicalSubsystem());
+    }
+
+    @Override
+    protected void initialize() {
+        Robot.getTechnicalSubsystem().clearStickys();
+    }
+
+    @Override
+    protected void execute() {
+    }
 
     @Override
     protected boolean isFinished() {
