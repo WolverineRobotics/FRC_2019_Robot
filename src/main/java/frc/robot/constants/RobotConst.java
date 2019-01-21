15788f631f43f52519b02dc92d0da7a57b9d7268
Khalil-Balde <KHALIL12345385@gmail.com>
2019-01-21 03:11:02 -0500

    added robot const

---
+++ src/main/java/frc/robot/constants/RobotConst.java
@@ -0,0 +1,16 @@
+package frc.robot.constants;
+
+public class RobotConst{
+    //********************************************************************************** 
+    // Drive constants
+    //**********************************************************************************
+    public static final int DRIVE_ENCODER_COUNTS_PER_INCH_HIGH = 1;
+    public static final int DRIVE_ENCODER_COUNTS_PER_INCH_LOW  = 1;
+    
+    //********************************************************************************** 
+    // Elevator constants
+    //**********************************************************************************
+    public static final int ELEVATOR_ENCODER_COUNTS_PER_INCH_HIGH = 1;
+    public static final int ELEVATOR_ENCODER_COUNTS_PER_INCH_LOW  = 1;
+
+}
