b99ed8b3c90a98203fe30fc3b1afcfc935911b94
Khalil Balde <Khalil12345385@gmail.com>
2019-02-04 12:05:51 -0500

    Added driver assistance

--- src/main/java/frc/robot/commands/defaultDriveCommand.java
+++ src/main/java/frc/robot/commands/defaultDriveCommand.java
@@ -2,16 +2,26 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import frc.robot.constants.RobotPIDValues;
 import frc.robot.oi.OI;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.util.PID;
 
 public class defaultDriveCommand extends Command {
     private DriveSubsystem c_drive = Robot.getDriveSubsystem();
+    private PID gyroPid =
+        new PID(RobotPIDValues.GYRO_KP,
+                RobotPIDValues.GYRO_KI,
+                RobotPIDValues.GYRO_KD, 0);
 
     public defaultDriveCommand() {
         requires(c_drive);
     }
 
+    @Override
+    protected void initialize() {
+        gyroPid.setMaxOutput(1);
+    }
     @Override
     protected void execute() {
         //driver input
@@ -27,6 +37,9 @@ public class defaultDriveCommand extends Command {
         double rightSpeed = 0;
         double slowDownSpeed = 0.5;
         boolean stopped = false;
+        double heading;
+        double currentHeading = Robot.getDriveSubsystem().getGyroAngle();
+        double steering = gyroPid.calcPID(currentHeading);
 
         //trigger for throttle
         if(Math.abs(throttle) < throttleTriggerValue){
@@ -43,6 +56,8 @@ public class defaultDriveCommand extends Command {
             stopped = true;
         } else {
             turn = OI.getTurn();
+            heading = c_drive.getGyroAngle();
+            gyroPid.setDesiredValue(heading);
             stopped = false;
         }
 
@@ -51,25 +66,44 @@ public class defaultDriveCommand extends Command {
         rightSpeed = throttle - turn;
 
         //max limit
-        if(leftSpeed > 1){
+        if (leftSpeed > 1) {
             leftSpeed = 1;
+        }
+        
+        if (rightSpeed > 1) {
+            rightSpeed = 1;
         }
 
         //min limit
-        if(leftSpeed < -1){
+        if (leftSpeed < -1) {
+            leftSpeed = -1;
+        }
+        
+        if (leftSpeed < -1) {
             leftSpeed = -1;
         }
 
         //Slow to stop
-        if(stopped){
+        if (stopped) {
             leftSpeed *= slowDownSpeed;
             rightSpeed *= slowDownSpeed;
         }
+        
+        //steering correction
+        if(steering < 0){
+            rightSpeed *= 1 - rightSpeed;
+        }
+
+        if(steering > 0){
+            leftSpeed *= 1 - leftSpeed;
+        }
+        
+        //move robot
+        c_drive.setRawSpeeds(leftSpeed, rightSpeed);
     }
 
     @Override
     protected boolean isFinished() {
         return false;
     }
-    
 }
