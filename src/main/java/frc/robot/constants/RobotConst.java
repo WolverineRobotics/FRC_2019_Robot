b911cd48f3580619050fc7c3d6ffe00ce3f421ad
Jeremy <madivoso@gmail.com>
2019-03-24 19:26:09 -0400

    RobotConst update
    - Removed some //TBD comments
    - Changed some values.
    - Added and removed some values.

--- src/main/java/frc/robot/constants/RobotConst.java
+++ src/main/java/frc/robot/constants/RobotConst.java
@@ -1,14 +1,14 @@
 package frc.robot.constants;
 
-public class RobotConst{
+public class RobotConst {
     //********************************************************************************** 
     // Drive constants
     //**********************************************************************************
-    public static final int DRIVE_ENCODER_COUNTS_PER_INCH_HIGH = 1; //TBD
-    public static final int DRIVE_ENCODER_COUNTS_PER_INCH_LOW  = 1; //TBD
+    public static final int DRIVE_ENCODER_COUNTS_PER_INCH_HIGH = 0; //TBD
+    public static final int DRIVE_ENCODER_COUNTS_PER_INCH_LOW  = 0; //TBD
 
-    public static final double DRIVE_TURN_TRIGGER_VALUE = 0.1; //TBD
-    public static final double DRIVE_THORTTLE_TRIGGER_VALUE = 0.2; //TBD
+    public static final double DRIVE_TURN_TRIGGER_VALUE = 0.1;
+    public static final double DRIVE_THORTTLE_TRIGGER_VALUE = 0.2;
 
     public static final double DRIVE_SPEED_REDUCTION_RATIO = 0.8;
     
@@ -19,30 +19,31 @@ public class RobotConst{
     public static final int ELEVATOR_ENCODER_COUNTS_PER_INCH_LOW = 1; //TBD
 
     public static final int ELEVATOR_SAFE_LEVEL_HEIGHT = 0;
-    public static final int ELEVATOR_FIRST_LEVEL_HEIGHT  = 0; //TBD
-    public static final int ELEVATOR_SECOND_LEVEL_HEIGHT = 0; //TBD
-    public static final int ELEVATOR_THIRD_LEVEL_HEIGHT  = 0; //TBD
 
-    public static final int ELEVATOR_MAX_HEIGHT = 0; //TBD
+    public static final int ELEVATOR_HATCH_FIRST_LEVEL_HEIGHT  = 0; //TBD
+    public static final int ELEVATOR_HATCH_SECOND_LEVEL_HEIGHT = 0; //TBD
+    public static final int ELEVATOR_HATCH_THIRD_LEVEL_HEIGHT = 0; //TBD
+    
+    public static final int ELEVATOR_CARGO_FIRST_LEVEL_HEIGHT = 0; //TBD
+    public static final int ELEVATOR_CARGO_SECOND_LEVEL_HEIGHT = 0; //TBD
+    public static final int ELEVATOR_CARGO_THIRD_LEVEL_HEIGHT = 0; //TBD
 
-    public static double ELEVATOR_LEFT_STICK_Y_TRIGGER_VALUE = 0.3; //TBD
+    public static final int ELEVATOR_MAX_ENCODER_HEIGHT = 0; //TBD
+
+    public static double ELEVATOR_LEFT_STICK_Y_TRIGGER_VALUE = 0.2;
+    public static double ELEVATOR_RIGHT_STICK_Y_TRIGGER_VALUE = 0.2;
     
     //********************************************************************************** 
     // Intake constants
     //**********************************************************************************
-    public static final double INTAKE_ELBOW_ENCODER_MAXIMUM = 0; //TBD
-    public static final double INTAKE_ELBOW_ENCODER_MINIMUM = 0; //TBD
+    public static final int INTAKE_ROTATE_ENCODER_MAXIMUM = 0; //TBD
+    public static final int INTAKE_ROTATE_ENCODER_MINIMUM = 0; //TBD
 
-    public static final double INTAKE_ELBOW_DEADBAND = 0; //TBD
-
-    public static final double RUMBLE_INTENSITY = 1; //When operator is intaking, driver controller will rumble. I assume that 1 is the max value?
+    public static final double INTAKE_ROTATE_TRIGGER_VALUE = 0.2;
 
     //********************************************************************************** 
     // Climb constants
     //**********************************************************************************
-
-    //********************************************************************************** 
-    // Vision constants
-    //**********************************************************************************
+    public static final double CLIMB_THROTTLE_TRIGGER_VALUE = 0.05;
 
 }
