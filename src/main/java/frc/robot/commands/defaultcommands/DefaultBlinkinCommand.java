b0db0d2255e3339dba2fee7afce29fedef4e6272
Jeremy <madivoso@gmail.com>
2019-03-24 19:27:24 -0400

    Rip DefaultBlinkCommand
    - Khalil deleted all of our blink stuff Scott
    - We have to get him back for this >:(

--- src/main/java/frc/robot/commands/defaultcommands/DefaultBlinkinCommand.java
+++ src/main/java/frc/robot/commands/defaultcommands/DefaultBlinkinCommand.java
@@ -1,12 +1,7 @@
 package frc.robot.commands.defaultcommands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.Robot;
-import frc.robot.commands.teleopcommands.BlinkCommand;
-import frc.robot.commands.teleopcommands.StaticCommand;
-import frc.robot.constants.blinkin.LEDCombo;
-import frc.robot.oi.OI;
 import frc.robot.subsystems.BlinkinSubsystem;
 
 public class DefaultBlinkinCommand extends Command {
@@ -20,27 +15,6 @@ public class DefaultBlinkinCommand extends Command {
 
     @Override
     protected void execute() {
-        if (OI.getDriverCancel()) { //B Button
-            if(c_blinkin.getCurrentLEDCombo() != LEDCombo.STATIC){
-                Scheduler.getInstance().add(new StaticCommand(LEDCombo.STATIC.getColour1()));
-                c_blinkin.setCurrentLEDCombo(LEDCombo.STATIC);
-            }
-        } else if (OI.getDriverRequestCargoLED()) { //A Button
-            if(c_blinkin.getCurrentLEDCombo() == LEDCombo.STATIC){
-                Scheduler.getInstance().add(new BlinkCommand(250, LEDCombo.REQUEST_CARGO.getColour1(), LEDCombo.REQUEST_CARGO.getColour2()));
-                c_blinkin.setCurrentLEDCombo(LEDCombo.REQUEST_CARGO);
-            }
-        } else if (OI.getDriverRequestionHatchLED()) { //X Button
-            if(c_blinkin.getCurrentLEDCombo() == LEDCombo.STATIC){
-                Scheduler.getInstance().add(new BlinkCommand(250, LEDCombo.REQUEST_HATCH.getColour1(), LEDCombo.REQUEST_HATCH.getColour2()));
-                c_blinkin.setCurrentLEDCombo(LEDCombo.REQUEST_HATCH);
-            }
-        } else if (OI.getDriverClimbing()) { //Y Button
-            if(c_blinkin.getCurrentLEDCombo() == LEDCombo.STATIC){
-                Scheduler.getInstance().add(new BlinkCommand(500, LEDCombo.CLIMBING.getColour1(), LEDCombo.CLIMBING.getColour2()));
-                c_blinkin.setCurrentLEDCombo(LEDCombo.CLIMBING);
-            }
-        }
     }
 
     /**
