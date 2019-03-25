858095b6d22b9a4cc4400678de69c20523f7e138
Scott <scottwork321@gmail.com>
2019-03-25 18:38:28 -0400

    Added Deadzone / Motor Limits to Util
    Still not used in the code yet

--- src/main/java/frc/util/Util.java
+++ src/main/java/frc/util/Util.java
@@ -10,6 +10,49 @@ public class Util{
         Scheduler.getInstance().add(command1);
     }
 
+    //Allows for input values less than trigger values. 
+    //Piecewise function, if less than deadzone value, 0        
+    //Else linear
+    //https://www.desmos.com/calculator/h7gnlk21iz 
+    
+    public static double getDeadzoneResult(double controlInput, double deadzoneAmount){
+
+        if(Math.abs(controlInput) < deadzoneAmount){
+            controlInput = 0;
+        }else {
+            if(controlInput>0){
+                controlInput = ((controlInput - deadzoneAmount)/(1-deadzoneAmount));
+            }else{
+                controlInput = ((controlInput + deadzoneAmount)/(1-deadzoneAmount));
+            }  
+        }
+        return controlInput;
+    }
+
+
+
+    //Ensures motor power values are between -1 and 1
+    public static double getMotorLimits(double motorPower){
+
+        if (motorPower < -1){
+            motorPower = -1;
+        }else if(motorPower > 1){
+            motorPower = 1;
+        }
+
+        return motorPower;
+    }
+
+
+   public static double setDeadzoneLimits(double controlInput, double deadzoneValue){
+        
+        double output = controlInput;
+        output = TriggerDeadzone.getDeadzoneResult(output, deadzoneValue);
+        output = MotorLimits.getMotorLimits(output);
+        return output;
+        
+
+    }
 
     
 
