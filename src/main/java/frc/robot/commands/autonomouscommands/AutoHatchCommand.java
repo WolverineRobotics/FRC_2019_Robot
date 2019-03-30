6bd645fd9f2153d58a680e28e5398551ff726ccb
Jeremy <madivoso@gmail.com>
2019-03-30 13:20:39 -0400

    Created inner class "GrabHatchCommadnGroup"
    This inner CommandGroup will simply latch the hatch into its proper position. Only run this once the hatch is within the  shovel after intaking with rollers.

--- src/main/java/frc/robot/commands/autonomouscommands/AutoHatchCommand.java
+++ src/main/java/frc/robot/commands/autonomouscommands/AutoHatchCommand.java
@@ -1,9 +1,9 @@
 package frc.robot.commands.autonomouscommands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.Robot;
-import frc.robot.commands.commandgroups.GrabHatchCommandGroup;
 import frc.robot.oi.OI;
 import frc.robot.subsystems.IntakeSubsystem;
 
@@ -35,6 +35,14 @@ public class AutoHatchCommand extends Command {
     @Override
     protected boolean isFinished() {
         return isDone;
+    }
+
+    class GrabHatchCommandGroup extends CommandGroup {
+        public GrabHatchCommandGroup() {
+            addParallel(new OpenShovelCommand(false));
+            addSequential(new SetIntakeRotateCommand(-20, 0.9));
+            addSequential(new OpenClawCommand(false));
+        }
     }
 
 }
