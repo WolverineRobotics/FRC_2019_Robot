edcb8ea7f01e1a8a0ab92627bd5014a147198e62
Scott <scottwork321@gmail.com>
2019-04-01 15:22:20 -0400

    Edited ClimbCommandGroup
    Now uses new gyro commands

--- src/main/java/frc/robot/commands/commandgroups/ClimbCommandGroup.java
+++ src/main/java/frc/robot/commands/commandgroups/ClimbCommandGroup.java
@@ -3,9 +3,11 @@ package frc.robot.commands.commandgroups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.commands.autonomouscommands.SetElevatorCommand;
 import frc.robot.commands.autonomouscommands.SetIntakeRotateCommand;
+import frc.robot.commands.autonomouscommands.SetIntakeRotateGyroCommand;
 import frc.robot.commands.autonomouscommands.ClimbLockCommand;
 import frc.robot.commands.autonomouscommands.SetClimbCommand;
 // import frc.robot.commands.autonomousfunctions.SetElevatorCommand;
+import frc.robot.commands.autonomouscommands.SetClimbGyroCommand;
 
 
 //Presses Y, elevator full down, lock, intake full back
@@ -30,8 +32,8 @@ public class ClimbCommandGroup extends CommandGroup{
         addSequential(new SetIntakeRotateCommand(0, 0.4)); //TODO: Change speed to actual value
         addSequential(new ClimbLockCommand(true));
 
-        addParallel(new SetIntakeRotateCommand(0, intakeSpeed, true)); // TODO:Change to encoder value
-        addSequential(new SetClimbCommand(0, climbSpeed, true)); //TODO:Change to encoder value
+        addParallel(new SetIntakeRotateGyroCommand(0.6, -10, true)); // TODO:Change to encoder value
+        addSequential(new SetClimbGyroCommand(0.6, -10, true)); //TODO:Change to encoder value
         
     }
 
