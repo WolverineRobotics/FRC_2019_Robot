ac54dd1d70f8147e417f72c57926b3f4c55431d0
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2019-04-02 10:11:22 -0400

    changed auto seletor logic

--- src/main/java/frc/robot/oi/AutoSelector.java
+++ src/main/java/frc/robot/oi/AutoSelector.java
@@ -3,54 +3,106 @@ package frc.robot.oi;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+@SuppressWarnings("deprecation")
 public class AutoSelector {
     private static SendableChooser<String> startPosition;
-    private static SendableChooser<String> objective;
+    private static SendableChooser<String> firstHatch;
+    private static SendableChooser<String> secondHatch;
+
 
     //starting positions
-    public static final String LEFT_LEVEL_1 = "L1";
-    public static final String LEFT_LEVEL_2 = "L2";
-    public static final String MIDDLE_LEVEL_1 = "M1";
-    public static final String RIGHT_LEVEL_1 = "R1";
-    public static final String RIGHT_LEVEL_2 = "R2";
+    public static final String POS_LEFT = "Left";
+    public static final String POS_RIGHT = "Right";
 
-    //objectives
-    public static final String ROCKET = "r";
-    public static final String CARGO_SHIP_FRONT = "c1";
-    public static final String CARGO_SHIP_BOTH = "c2";
-    public static final String CARGO_SHIP_SIDE = "c3";
-    public static final String CARGO_SHIP_BACK = "c4";
+    public static final String CS_FRONT = "Front cargo ship";
+    public static final String CS_SIDE1 = "Close cargo ship";
+    public static final String CS_SIDE2 = "Middle cargo ship";
+    public static final String CS_SIDE3 = "Far cargo ship";
 
-    public static final String BASELINE = "b";
+    public static final String RS_CLOSE1 = "Close level 1 rocket";
+    public static final String RS_CLOSE2 = "Close level 2 rocket";
+    public static final String RS_CLOSE3 = "Close level 3 rocket";
+    public static final String RS_FAR1 = "Far level 1 rocket";
+    public static final String RS_FAR2 = "Far level 2 rocket";
+    public static final String RS_FAR3 = "Far level 3 rocket";
+
+    public static final String NONE = "nothing";
+
+    private static String position;
+    private static String firstAction;
+    private static String secondAction;
 
     static {
         startPosition = new SendableChooser<>();
+        firstHatch = new SendableChooser<>();
+        secondHatch = new SendableChooser<>();
 
-        startPosition.setDefaultOption("Left level 1", LEFT_LEVEL_1);
-        startPosition.addOption("Left level 2", LEFT_LEVEL_2);
-        startPosition.addOption("Middle", MIDDLE_LEVEL_1);
-        startPosition.addOption("Right level 1", RIGHT_LEVEL_1);
-        startPosition.addOption("Right level 2", RIGHT_LEVEL_2);
+        //********************************************************************************** 
+        // start position
+        //**********************************************************************************
+        startPosition.addDefault(POS_LEFT, POS_LEFT);
+        startPosition.addObject(POS_RIGHT, POS_RIGHT);
 
-        objective = new SendableChooser<>();
-        objective.setDefaultOption("Baseline", BASELINE);
-        objective.addOption("Cargo ship 2 front hatches", CARGO_SHIP_FRONT);
-        objective.addOption("Cargo ship 2 close side hatches", CARGO_SHIP_SIDE);
-        objective.addOption("Cargo ship 2 far side hatches", CARGO_SHIP_BACK);
-        objective.addOption("Cargo ship 1 front 1 side hatch", CARGO_SHIP_BOTH);
-        objective.addOption("Rocket bottom 2", ROCKET);
+        //********************************************************************************** 
+        // first hatch
+        //**********************************************************************************
+        firstHatch.addDefault(NONE, NONE);
+        firstHatch.addObject(CS_FRONT, CS_FRONT);
+        firstHatch.addObject(CS_SIDE1, CS_SIDE1);
+        firstHatch.addObject(CS_SIDE2, CS_SIDE2);
+        firstHatch.addObject(CS_SIDE3, CS_SIDE3);
 
+        firstHatch.addObject(RS_CLOSE1, RS_CLOSE1);
+        firstHatch.addObject(RS_CLOSE2, RS_CLOSE2);
+        firstHatch.addObject(RS_CLOSE2, RS_CLOSE2);
+
+        firstHatch.addObject(RS_FAR1, RS_FAR1);
+        firstHatch.addObject(RS_FAR2, RS_FAR2);
+        firstHatch.addObject(RS_FAR3, RS_FAR3);
+
+        //********************************************************************************** 
+        // second hatch
+        //**********************************************************************************
+        secondHatch.addDefault(NONE, NONE);
+        secondHatch.addObject(CS_FRONT, CS_FRONT);
+        secondHatch.addObject(CS_SIDE1, CS_SIDE1);
+        secondHatch.addObject(CS_SIDE2, CS_SIDE2);
+        secondHatch.addObject(CS_SIDE3, CS_SIDE3);
+
+        secondHatch.addObject(RS_CLOSE1, RS_CLOSE1);
+        secondHatch.addObject(RS_CLOSE2, RS_CLOSE2);
+        secondHatch.addObject(RS_CLOSE2, RS_CLOSE2);
+
+        secondHatch.addObject(RS_FAR1, RS_FAR1);
+        secondHatch.addObject(RS_FAR2, RS_FAR2);
+        secondHatch.addObject(RS_FAR3, RS_FAR3);
+
+        //********************************************************************************** 
+        // put on smartdashboard
+        //**********************************************************************************
         SmartDashboard.putData(startPosition);
-        SmartDashboard.putData(objective);
+        SmartDashboard.putData(firstHatch);
+        SmartDashboard.putData(secondHatch);
+        
+        position = startPosition.getSelected();
+        firstAction = firstHatch.getSelected();
+        secondAction = secondHatch.getSelected();
+
+        //********************************************************************************** 
+        // autonomous choice overrides
+        //**********************************************************************************
+        
     }
 
-    public static String getStartingPosition(){
-        return startPosition.getSelected();
+    public static String getPosition(){
+        return position;
     }
 
-    public static String getObjective(){
-        return objective.getSelected();
+    public static String getFirstAction(){
+        return firstAction;
     }
-    
 
+    public static String getSecondAction(){
+        return secondAction;
+    }
 }
